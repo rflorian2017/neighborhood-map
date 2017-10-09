f96a9cdf86a8eb873ff9932a891ad1cc46f40792
rflorian2017 <roby_florian@yahoo.com>
2017-10-09 23:06:05 +0300

    Second commit
    Added infowindow

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ var initialLocations = [
 			lat:44.629859,
 			lng:22.6499349
 		},
-		caption: 'Water Castle'
+		caption: 'Water Castle Drobeta'
 	}, 
 	{
 		title: 'Cinetic Fountain',
@@ -20,14 +20,18 @@ var initialLocations = [
 ];
 
 var map;
+var infowindow;
 
 /* Dr. Babes street, the place where I spent my childhood. It has wonderful memories.
 Thanks to my parents, especially to my mom, who made me what I am today ! */
 var myHomeLocation = {lat: 44.6335713, lng: 22.6699706}; 
 
 var NeighborhodLocation = function(data) {
+	var self = this;
 	this.title = data.title;
 	this.location = data.location;
+	this.visible = true;
+	this.caption = data.caption;
 	
 	this.marker = new google.maps.Marker({
             position: this.location,
@@ -35,26 +39,37 @@ var NeighborhodLocation = function(data) {
 			animation: google.maps.Animation.DROP
     });
 	
+	this.marker.addListener('click', function() {
+            self.populateInfoWindow(self.marker,infowindow);
+    });
+	
 	this.marker.setMap(map);
 	
-	this.populateInfoWindow = function(infowindow) {
+	$.getJSON(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=314de7a5eaa02b76ab2438beaefe1898&text=${this.caption}&format=json`).done(function(data) {
+		console.log(data);
+	});
+	
+	this.populateInfoWindow = function() {
 		console.log("first");
-		if (this.marker.getAnimation() !== null) {
-          this.marker.setAnimation(null);
+		if (self.marker.getAnimation() !== null) {
+          self.marker.setAnimation(null);
         } else {
-          this.marker.setAnimation(google.maps.Animation.BOUNCE);
+          self.marker.setAnimation(google.maps.Animation.BOUNCE);
+		  setTimeout(function() {
+			  self.marker.setAnimation(null);
+		  },1500);
         }
         // Check to make sure the infowindow is not already opened on this marker.
-        // if (infowindow.marker != marker) {
-          // infowindow.marker = marker;
-          // infowindow.setContent('<div>' + marker.title + '</div>');
-          // infowindow.open(map, marker);
+        if (infowindow.marker != self.marker) {
+			infowindow.marker = self.marker;
+            infowindow.setContent('<div>' + this.marker.title + '</div>');
+            infowindow.open(map, this.marker);
           // Make sure the marker property is cleared if the infowindow is closed.
-          // infowindow.addListener('closeclick', function() {
-            // infowindow.marker = null;
-			// marker.setAnimation(null);
-          // });
-        // }
+            infowindow.addListener('closeclick', function() {
+				infowindow.marker = null;
+				self.marker.setAnimation(null);
+			});
+        }
     }
 }
 	
@@ -67,37 +82,22 @@ function initMap() {
     zoom: 14
     });
        
-	// for (var i = 0; i < initialLocations.length; i++) {
-		
-        // Push the marker to our array of markers.
-        // markers.push(marker);
-		// marker.setMap(map);
-		// var largeInfowindow = new google.maps.InfoWindow();
-		
-        // Create an onclick event to open the large infowindow at each marker.
-        // marker.addListener('click', function() {
-            // populateInfoWindow(this, largeInfowindow);
-        // });
-	// }
-	
 	ko.applyBindings(new ViewModel());
 }
 	 
 var ViewModel = function() {
 	var self = this;
 	this.neighborhoodLocations = ko.observableArray([]);
+	infowindow = new google.maps.InfoWindow();
 	
 	for(locationItem of initialLocations) {
 		self.neighborhoodLocations.push(new NeighborhodLocation(locationItem));
 	}
 	
+	this.currentLocation = ko.observable(this.neighborhoodLocations()[0]);
+	
 	this.filterResults = function() {
 		//todo
-	}
-	
-	this.populateMarkerInfo = function() {
-		self.printSomething();
-		
 	}
 	 
 	 //initMap();
