1b8b0a4a0ae393266ff03650de857aa46d0607f1
rflorian2017 <roby_florian@yahoo.com>
2017-10-11 00:17:44 +0300

    Added several functionalities

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var initialLocations = [
+const initialLocations = [
 	{
 		title: 'Water Tower',
 		location: 
@@ -6,7 +6,7 @@ var initialLocations = [
 			lat:44.629859,
 			lng:22.6499349
 		},
-		caption: 'Water Castle Drobeta'
+		caption: 'Turnul de apa'
 	}, 
 	{
 		title: 'Cinetic Fountain',
@@ -15,12 +15,14 @@ var initialLocations = [
 			lat:44.624336,
 			lng:22.6521189
 		},
-		caption: 'Fountain'
+		caption: 'Fantana cinetica'
 	}	
 ];
 
 var map;
 var infowindow;
+const clientId = "PLKEOUJEJ3BRRWWUDCPMBCPZHWOYKDGEHUDTPOZQPVFMGU15";
+const clientSecret = "ZS2XQVPW1JZLHQYRJD50JTX4PPNGV1LEENOEVJGVZSFM5L0U";
 
 /* Dr. Babes street, the place where I spent my childhood. It has wonderful memories.
 Thanks to my parents, especially to my mom, who made me what I am today ! */
@@ -30,8 +32,9 @@ var NeighborhodLocation = function(data) {
 	var self = this;
 	this.title = data.title;
 	this.location = data.location;
-	this.visible = true;
+	this.visible = ko.observable(true);
 	this.caption = data.caption;
+	this.venueName = '';
 	
 	this.marker = new google.maps.Marker({
             position: this.location,
@@ -44,13 +47,26 @@ var NeighborhodLocation = function(data) {
     });
 	
 	this.marker.setMap(map);
+	this.placeDescription = '';
 	
-	$.getJSON(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=314de7a5eaa02b76ab2438beaefe1898&text=${this.caption}&format=json`).done(function(data) {
-		console.log(data);
+	let foursquareRequestUr = 'https://api.foursquare.com/v2/venues/search?ll=' + this.location.lat+',' + this.location.lng + '&client_id='+ 
+			 clientId + '&client_secret=' + clientSecret + '&query=' + this.caption + '&v=20170801' + '&limit=1';
+		
+	$.getJSON(foursquareRequestUr).done(function(data) {
+		
+		self.venueName = data.response.venues[0].name;
+		self.placeDescription = data.response.venues[0].categories[0].name;
+		if(self.venueName==='undefined') {
+			self.venueName = self.title;
+		}
+	}).fail(function() {
+		this.contentOfInfoWindow = 'The request could not be completed';
 	});
 	
 	this.populateInfoWindow = function() {
-		console.log("first");
+		let contentOfInfoWindow = '';
+		let idOfVenue = '';
+		
 		if (self.marker.getAnimation() !== null) {
           self.marker.setAnimation(null);
         } else {
@@ -62,8 +78,8 @@ var NeighborhodLocation = function(data) {
         // Check to make sure the infowindow is not already opened on this marker.
         if (infowindow.marker != self.marker) {
 			infowindow.marker = self.marker;
-            infowindow.setContent('<div>' + this.marker.title + '</div>');
-            infowindow.open(map, this.marker);
+            infowindow.setContent('<h4>' + self.venueName + '</h4><div>' + self.placeDescription + '<div>');
+            infowindow.open(map, self.marker);
           // Make sure the marker property is cleared if the infowindow is closed.
             infowindow.addListener('closeclick', function() {
 				infowindow.marker = null;
@@ -71,6 +87,15 @@ var NeighborhodLocation = function(data) {
 			});
         }
     }
+	
+	this.toggleVisibility = ko.computed(function() {
+		if(this.visible()===true) {
+			this.marker.setMap(map);
+		}
+		else {
+			this.marker.setMap(null);
+		}
+	},this);
 }
 	
 function initMap() {
@@ -89,17 +114,22 @@ var ViewModel = function() {
 	var self = this;
 	this.neighborhoodLocations = ko.observableArray([]);
 	infowindow = new google.maps.InfoWindow();
+	this.filterForLocations = ko.observable("");
 	
 	for(locationItem of initialLocations) {
 		self.neighborhoodLocations.push(new NeighborhodLocation(locationItem));
 	}
 	
-	this.currentLocation = ko.observable(this.neighborhoodLocations()[0]);
-	
-	this.filterResults = function() {
-		//todo
-	}
-	 
-	 //initMap();
-	
+	this.filterLocations = ko.pureComputed(function() {
+		var filter = self.filterForLocations();
+        if(!filter) {
+            return self.neighborhoodLocations(); 
+        } else {
+            return ko.utils.arrayFilter(self.neighborhoodLocations(), function(item) {
+				let filterResult = item.title.toLowerCase().includes(filter);
+				item.visible(filterResult);
+                return filterResult;
+            });
+        }
+    },self);	
 };
