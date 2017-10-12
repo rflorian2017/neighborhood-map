5a7b5c8578984dca233fb4481be07e99a8e69535
rflorian2017 <30376759+rflorian2017@users.noreply.github.com>
2017-10-12 09:53:07 +0300

    Corrections after the review
    Corrections after the review

--- js/app.js
+++ js/app.js
@@ -1,61 +1,3 @@
-/* create an array with the initial locations */
-const initialLocations = [{
-        title: 'Water Tower',
-        location: {
-            lat: 44.629859,
-            lng: 22.6499349
-        },
-        caption: 'Turnul de apa drobeta',
-        captionFlickr: 'castelul de apa drobeta'
-    },
-    {
-        title: 'Cinetic Fountain',
-        location: {
-            lat: 44.624336,
-            lng: 22.6521189
-        },
-        caption: 'Fantana cinetica drobeta',
-        captionFlickr: 'Fantana cinetica drobeta'
-    },
-    {
-        title: '16,50',
-        location: {
-            lat: 44.6256027,
-            lng: 22.6477736
-        },
-        caption: '16,50 Drobeta',
-        captionFlickr: 'Restaurant 16,50 Drobeta'
-    },
-    {
-        title: 'Railroad station',
-        location: {
-            lat: 44.6220939,
-            lng: 22.6419639
-        },
-        caption: 'Gara Drobeta',
-        captionFlickr: 'Gara Drobeta'
-    },
-    {
-        title: 'Traian\'s bridge',
-        location: {
-            lat: 44.6237118,
-            lng: 22.6649434
-        },
-        caption: 'Piciorul podului',
-        captionFlickr: 'Traian bridge drobeta'
-    },
-    {
-        title: 'Iron Gates Museum',
-        location: {
-            lat: 44.6237118,
-            lng: 22.6649434
-        },
-        caption: 'Iron Gates Museum Drobeta',
-        captionFlickr: 'Muzeu Portile de Fier drobeta'
-    }
-
-];
-
 let map;
 let infowindow;
 /* client Id and secret for Foursquare API request */
@@ -209,5 +151,5 @@ var ViewModel = function() {
 };
 
 function errorCall() {
-    document.getElementById("map").innerHTML = "The map could not be loaded";
+	alert("The map could not be loaded!");
 }
---
+++ js/locations.js
@@ -0,0 +1,63 @@
+/* create an array with the initial locations */
+const initialLocations = [
+	{
+        "title": "Water Tower",
+        "location": 
+		{
+            "lat": 44.629859,
+            "lng": 22.6499349
+        },
+        "caption": "Turnul de apa drobeta",
+        "captionFlickr": "castelul de apa drobeta"
+    },
+    {
+        "title": "Cinetic Fountain",
+        "location": 
+		{
+            "lat": 44.624336,
+            "lng": 22.6521189
+        },
+        "caption": "Fantana cinetica drobeta",
+        "captionFlickr": "Fantana cinetica drobeta"
+    },
+    {
+        "title": "16,50",
+        "location": 
+		{
+            "lat": 44.6256027,
+            "lng": 22.6477736
+        },
+        "caption": "16,50 Drobeta",
+        "captionFlickr": "Restaurant 16,50 Drobeta"
+    },
+    {
+        "title": "Railroad station",
+        "location": 
+		{
+            "lat": 44.6220939,
+            "lng": 22.6419639
+        },
+        "caption": "Gara Drobeta",
+        "captionFlickr": "Gara Drobeta"
+    },
+    {
+        "title": "Traian's bridge",
+        "location": 
+		{
+            "lat": 44.6237118,
+            "lng": 22.6649434
+        },
+        "caption": "Piciorul podului",
+        "captionFlickr": "Traian bridge drobeta"
+    },
+    {
+        "title": "Iron Gates Museum",
+        "location": 
+		{
+            "lat": 44.6237118,
+            "lng": 22.6649434
+        },
+        "caption": "Iron Gates Museum Drobeta",
+        "captionFlickr": "Muzeu Portile de Fier drobeta"
+    }
+];
